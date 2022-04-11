c6bcd840b167dae3d66a1cccf62cd2c818fa52ae
GabrielFermino <gabriel.cf2009@hotmail.com>
2022-04-11 20:11:12 -0300

    fist commit

---
+++ driver.py
@@ -0,0 +1,46 @@
+import driver
+import sys
+import shutil
+from pyke import knowledge_engine, krb_traceback
+
+engine = knowledge_engine.engine(__file__)
+
+
+# def bc_test():
+#     engine.reset()
+
+#     engine.activate('rules')
+
+#     try:
+#         with engine.prove_goal('rules.animal_class($class)') as gen:
+#             for vars, plan in gen:
+#                 print("O animal é: %s" % (vars['class']))
+
+#     except Exception:
+
+#         krb_traceback.print_exc()
+#         sys.exit(1)
+
+#     print("Programa Finalizado!")
+
+
+def bc_test_question():
+    engine.reset()
+
+    engine.activate('more_rules')
+
+    try:
+        with engine.prove_goal('more_rules.animal_class($class)') as gen:
+            for vars, plan in gen:
+                print("O animal é: %s" % (vars['class']))
+
+    except:
+        print("Descricao incompativel")
+        krb_traceback.print_exc()
+        sys.exit(1)
+
+    print("Programa Finalizado!")
+
+
+# shutil.rmtree('./compiled_krb')
+# driver.bc_test_question()
